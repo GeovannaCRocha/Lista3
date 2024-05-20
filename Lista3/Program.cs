fe17fcd0d7e85884ccfb2555acf9ed29a8745f7b
GeovannaCosta <158053742+GeovannaCosta@users.noreply.github.com>
2024-05-20 15:54:00 -0300

    Adicionar arquivos de projeto.

---
+++ Lista3/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+    namespace lista3 {
+         class Program {
+   
+        static void Main(string[] args) {
+
+            /*Console.WriteLine("Digite o número do funcionário:");
+            int numeroFuncionario = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o número de horas trabalhadas:");
+            int horasTrabalhadas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor que o funcionário recebe por hora:");
+            double valorPorHora = double.Parse(Console.ReadLine());
+
+            double salario = horasTrabalhadas * valorPorHora;
+
+            Console.WriteLine($"Número do funcionário: {numeroFuncionario}");
+            Console.WriteLine($"Salário do funcionário: R$ {salario:F2}");*/
+
+
+
+            /*Console.WriteLine("Informe o código da peça 1:");
+            int codigoPeca1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o número de peças 1:");
+            int numeroPecas1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o valor do unitário da peça 1:");
+            double valorUnitarioPeca1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o código da pelça 2:");
+            int codigoPeca2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o número de peças 2:");
+            int numeroPecas2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o valor do unitário da peça 2:");
+            double valorUnitarioPeca2 = double.Parse(Console.ReadLine());
+
+            double totalPeca1 = numeroPecas1 * valorUnitarioPeca1;
+            double totalPeca2 = numeroPecas2 * valorUnitarioPeca2;
+            double valorTotal = totalPeca1 * totalPeca2;
+
+            Console.WriteLine($"O valor total ser pago é: R$ {valorTotal:F2}");*/
+
+
+
+           /* Console.WriteLine("Digite o valor de A:");
+            double A = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de B:");
+            double B = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor de C:");
+            double C = double.Parse(Console.ReadLine());
+
+            double areaTriangulo = A * C / 2.0;
+            Console.WriteLine($"Área do triângulo retângulo: {areaTriangulo}");
+            double areaCirculo = 3.14159 * C * C;
+            Console.WriteLine($"Área do circulo: {areaCirculo}");
+            double areaTrapezio = (A + B) / 2.0 * C;
+            Console.WriteLine($"Área do trapezio: {areaTrapezio}"); 
+            double areaQuadrado = B * B;
+            Console.WriteLine($"Área do quadrado: {areaQuadrado}");
+            double areaRetangulo = A * B;
+            Console.WriteLine($"Área do retangulo: {areaRetangulo}");*/
+        }
+    }
+ }
